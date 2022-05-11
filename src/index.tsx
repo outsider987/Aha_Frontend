4b4ac2200dd5eb6d055029c9c6c9f2cf5422e1ae
Victor <victor_chang@mediconcen.com>
2022-05-12 01:34:22 +0800

    feat: add eslint plugin is google

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { MYRoutes } from './router';
+import {BrowserRouter} from 'react-router-dom';
+import {MYRoutes} from './router';
 import './index.scss';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <MYRoutes />
-        </BrowserRouter>
+      <BrowserRouter>
+        <MYRoutes />
+      </BrowserRouter>
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById('root'),
 );
