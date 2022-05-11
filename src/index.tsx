bcb04084aa11be5cc65d819eb5493b507257c285
Victor <victor_chang@mediconcen.com>
2022-05-12 00:35:41 +0800

    1.initialize package tsconfig react structure

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import './index.scss';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter></BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
